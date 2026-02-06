4ea94c6f9f99640b45a0103c3a44ef32c13c9106
michał <51276321+michalzuch@users.noreply.github.com>
2026-02-06 16:20:34 +0100

    Add harmony target

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,26 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {mergeConfig, getDefaultConfig} = require('@react-native/metro-config');
+const {
+  createHarmonyMetroConfig,
+} = require('@react-native-oh/react-native-harmony/metro.config');
 
 /**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
+ * @type {import("metro-config").MetroConfig}
  */
-const config = {};
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(
+  getDefaultConfig(__dirname),
+  createHarmonyMetroConfig({
+    reactNativeHarmonyPackageName: '@react-native-oh/react-native-harmony',
+  }),
+  config,
+);
